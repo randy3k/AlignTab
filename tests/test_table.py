03763c14321f0adb923b6de933655f7126552882
Randy Lai <randy.cs.lai@gmail.com>
2014-11-05 16:58:25 -0800

    add latex table test

--- tests/test_table.py
+++ tests/test_table.py
@@ -36,3 +36,24 @@ class TestTable(DeferrableTestCase):
         yield 400
         second_row = self.getRow(2)
         self.assertEqual(second_row,"d     | e | f")
+
+    def test_latex_table(self):
+        string = """  \letter{S} \\ \hline
+    Some text                      & the second column & short \\ \hline
+    Some more text that is aligned & the second column & longer than short \\ \hline
+  \letter{T} \\ \hline
+    Totally related text & that is aligned differently, unfortunately & blah \\ \hline"""
+        self.setText(string)
+        self.view.run_command("select_all")
+        self.view.run_command("align_tab", {"user_input" :"&", "mode" : True})
+        history.insert("&")
+        self.view.sel().clear()
+        yield 400
+        self.view.sel().add(sublime.Region(0,0))
+        self.setText("\n")
+        self.view.sel().clear()
+        self.view.sel().add(sublime.Region(157,157))
+        self.setText("abc")
+        yield 400
+        second_row = self.getRow(6)
+        self.assertEqual(second_row,"      Totally related text              & that is aligned differently, unfortunately & blah \ \hline")
